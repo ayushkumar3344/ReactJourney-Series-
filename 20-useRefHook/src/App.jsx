e81fab9fd11c99e8d73a31487e218105e401739c
user.name : "Ayush <kumarayush8117@gmail.com>
2026-07-19 00:15:29 +0530

    About the useRef hook i understand about useRef it is mostly used to handle DOM manipulation in React.

--- 20-useRefHook/src/App.jsx
+++ 20-useRefHook/src/App.jsx
@@ -3,11 +3,24 @@ import React, { useRef } from 'react'
 const App = () => {
 
   const inputRef = useRef(null);
+  const h1Ref = useRef(null);
 
   const inputHandler = () => {
-    console.log(inputRef);
-    inputRef.current.focus();
     inputRef.current.style.color="Red";
+  }
+
+  const toggleHandler = () => {
+    
+    if( inputRef.current.style.display!="none" ){
+      inputRef.current.style.display="none"
+    } else {
+      inputRef.current.style.display="inline"
+    }
+
+  }
+
+  const h1Handler = () => {
+    h1Ref.current.style.fontSize="50px"
   }
 
   return (
@@ -16,6 +29,11 @@ const App = () => {
       <h1 className="text-3xl font-extrabold underline">
         UseRef Hook in React :
       </h1>
+
+      <button 
+      onClick={toggleHandler}
+      className='border border-red bg-gray-800 mr-5 py-1 px-2 rounded cursor-pointer'
+      >Toggle</button>
 
       <input 
       ref={inputRef}
@@ -28,6 +46,13 @@ const App = () => {
       onClick={inputHandler}
       className='border border-red bg-gray-800 ml-5 py-1 px-2 rounded cursor-pointer'
       >Focus On Input Field</button>
+
+      <h1 ref={h1Ref}>Hello : Ayush</h1>
+      <button 
+      onClick={h1Handler}
+      className='border border-red bg-gray-800 ml-5 py-1 px-2 rounded cursor-pointer'
+      >Trget H1</button>
+
 
     </div>
   )
